289d09227ba0ae20c1654bde6773efc45ad73aa7
gharghouchloubna <gharghouch.loubnaa@gmail.com>
2023-10-20 07:18:03 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
- *
- * Return: Always 0 (Success)
- */
+* main - 4-puts.c
+* description - Write a C program that prints exactly
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
